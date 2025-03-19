ea45fc4c0caaed7de68c87257a76e9df9c591861
Yasowant <yasowant1998@gmail.com>
2025-03-20 02:35:57 +0530

    updated code for login

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/header";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/header';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from 'sonner';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Vehiqle",
-  description: "Find your Dream Car",
+  title: 'Vehiqle',
+  description: 'Find your Dream Car',
 };
 
 export default function RootLayout({ children }) {
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
+              <p>Made with 💗 by MidNightCoder</p>
             </div>
           </footer>
         </body>
